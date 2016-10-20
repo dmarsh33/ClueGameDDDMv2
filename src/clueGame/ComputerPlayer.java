6fbb9934f17c76cb5a70cac942889d8f74193df7
Devon Dunmire <ddunmire@mymail.mines.edu>
2016-10-20 11:28:49 -0600

    added some files

---
+++ src/clueGame/ComputerPlayer.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+import java.util.*;
+
+public class ComputerPlayer extends Player{
+	public BoardCell pickLocation(Set<BoardCell> targets){
+		return null;
+	}
+	public void makeAccusation(){
+		
+	}
+	public void createSuggestion(){
+		
+	}
+}
