ec270179abf12be73c7feb34f0e15759b3396e93
Delaney Marsh <dmarsh@mymail.mines.edu>
2016-11-04 14:29:17 -0600

    MAde Solution variables private and added getters

--- src/clueGame/Solution.java
+++ src/clueGame/Solution.java
@@ -1,9 +1,21 @@
 package clueGame;
 
 public class Solution {
-	public String person;
-	public String weapon;
-	public String room;
+	private String person;
+	private String weapon;
+	private String room;
+	public String getPerson() {
+		return person;
+	}
+
+	public String getWeapon() {
+		return weapon;
+	}
+
+	public String getRoom() {
+		return room;
+	}
+
 	public Solution(String person, String weapon, String room) {
 		super();
 		this.person = person;
