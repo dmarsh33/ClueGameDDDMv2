1469a54d251a95bd32bacf36ed90d7bcf4fee2b6
Delaney Marsh <dmarsh@mymail.mines.edu>
2016-11-17 10:28:23 -0700

    Updated guessDialog so that the person the human guesses moves to the room they are accused in

---
+++ src/clueGame/AccusationDialog.java
@@ -0,0 +1,81 @@
+package clueGame;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class AccusationDialog extends JDialog{
+	Board board;
+	JComboBox<String> personSuggestion = new JComboBox<String>();
+	JComboBox<String> weaponSuggestion = new JComboBox<String>();
+	JTextField yourRoom = new JTextField(20);
+	JTextField person = new JTextField(20);
+	JTextField weapon = new JTextField(20);
+	JComboBox roomSuggestion = new JComboBox<String>();
+	JButton submitButton = new JButton("Submit");
+	JButton cancelButton = new JButton("Cancel");
+	
+	
+	public AccusationDialog(){
+		board = Board.getInstance();
+		setTitle("Make a Guess");
+		setSize(200,200);
+		setLayout(new GridLayout(4, 2));
+		for(Card person : board.getDeckPeople()){
+			personSuggestion.addItem(person.getCardName());
+		}
+		for(Card weapon : board.getDeckWeapons()){
+			weaponSuggestion.addItem(weapon.getCardName());
+		}
+		for(Card room : board.getDeckRooms()){
+			roomSuggestion.addItem(room.getCardName());
+		}
+		
+		yourRoom.setText("Your Room");
+		person.setText("Person");
+		weapon.setText("Weapon");
+	
+		
+		
+		submitButton.addActionListener(new SubmitButtonListener());
+		cancelButton.addActionListener(new CancelButtonListener());
+		
+		add(yourRoom);
+		add(roomSuggestion);
+		add(person);
+		add(personSuggestion);
+		add(weapon);
+		add(weaponSuggestion);
+		add(submitButton);
+		add(cancelButton);
+	}
+
+	
+	private class SubmitButtonListener implements ActionListener{ //listens to submit button click
+		public void actionPerformed(ActionEvent e){ 
+			String guessedPerson = personSuggestion.getSelectedItem().toString();
+			String guessedWeapon = weaponSuggestion.getSelectedItem().toString();
+			String guessedRoom = roomSuggestion.getSelectedItem().toString();
+			
+			board.getCurrentPlayer().setSolution(guessedPerson, guessedWeapon, guessedRoom);
+			Solution accusation = new Solution(guessedPerson, guessedWeapon, guessedRoom);
+			setVisible(false);
+			
+			if(accusation.equals(board.getSolution())){
+				JOptionPane.showMessageDialog(null, "That is correct! You won!");
+			}
+			else{
+				JOptionPane.showMessageDialog(null, "That is incorrect!");
+			}
+		}
+	}
+	
+	private class CancelButtonListener implements ActionListener{ //listens to submit button click
+		public void actionPerformed(ActionEvent e){ 
+			setVisible(false);
+		}
+	}
+}
---
+++ src/clueGame/GuessDialog.java
@@ -0,0 +1,86 @@
+package clueGame;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class GuessDialog extends JDialog{
+	Board board;
+	JComboBox<String> personSuggestion = new JComboBox<String>();
+	JComboBox<String> weaponSuggestion = new JComboBox<String>();
+	JTextField yourRoom = new JTextField(20);
+	JTextField person = new JTextField(20);
+	JTextField weapon = new JTextField(20);
+	JTextField room = new JTextField(20);
+	JButton submitButton = new JButton("Submit");
+	JButton cancelButton = new JButton("Cancel");
+	String returnedCard = "";
+	Solution newGuess = null;
+	String guessString = "";
+	
+	public GuessDialog(){
+		board = Board.getInstance();
+		setTitle("Make a Guess");
+		setSize(200,200);
+		setLayout(new GridLayout(4, 2));
+		for(Card person : board.getDeckPeople()){
+			personSuggestion.addItem(person.getCardName());
+		}
+		for(Card weapon : board.getDeckWeapons()){
+			weaponSuggestion.addItem(weapon.getCardName());
+		}
+		
+		yourRoom.setText("Your Room");
+		person.setText("Person");
+		weapon.setText("Weapon");
+		room.setText(board.getPlayersList().get(board.getPlayersList().size() - 1).getPlayerRoom());
+		
+		
+		submitButton.addActionListener(new SubmitButtonListener());
+		cancelButton.addActionListener(new CancelButtonListener());
+		
+		add(yourRoom);
+		add(room);
+		add(person);
+		add(personSuggestion);
+		add(weapon);
+		add(weaponSuggestion);
+		add(submitButton);
+		add(cancelButton);
+	}
+
+	
+	private class SubmitButtonListener implements ActionListener{ //listens to submit button click
+		public void actionPerformed(ActionEvent e){ 
+			String guessedPerson = personSuggestion.getSelectedItem().toString();
+			String guessedWeapon = weaponSuggestion.getSelectedItem().toString();
+			String guessedRoom = room.getText();
+			guessString = guessedPerson + ", " + guessedWeapon + ", " + guessedRoom;
+			newGuess = new Solution(guessedPerson, guessedWeapon, guessedRoom);
+			board.getCurrentPlayer().setSolution(guessedPerson, guessedWeapon, guessedRoom); //sets player's suggestion and guesstext
+			Card c = board.handleSuggestion(board.getPlayersList(), newGuess);
+			board.getCurrentPlayer().setDisprovingCard(c);
+			//returnedCard = c.getCardName();
+			setVisible(false);
+			Player guessedPlayer = board.getPlayers().get(guessedPerson);
+			guessedPlayer.setLocation(board.getPlayersList().get(board.getPlayersList().size() - 1).getRow(), board.getPlayersList().get(board.getPlayersList().size() - 1).getCol());
+		}
+	}
+	
+	private class CancelButtonListener implements ActionListener{ //listens to submit button click
+		public void actionPerformed(ActionEvent e){ 
+			setVisible(false);
+		}
+	}
+	
+	public String getReturnedCard(){
+		return returnedCard;
+	}
+	
+	public String getNewGuess(){
+		return guessString;
+	}
+}
